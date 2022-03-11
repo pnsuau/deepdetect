8e6c055446551cd698f51c23e6c136b684b9c7ca
pnsuau <pierre-nicolas.suau@jolibrain.com>
2022-03-11 17:19:19 +0100

    feat: saving crops and segmentation masks with sliding window script

--- demo/segmentation/segment_sliding.py
+++ demo/segmentation/segment_sliding.py
@@ -13,7 +13,8 @@ sname = 'imgserv'
 description = 'image segmentation'
 mllib = 'torch'
 mltype = 'supervised'
-dd = DD(host)
+path = '/api/private'
+dd = DD(host,port,0,path=path)
 dd.set_return_format(dd.RETURN_PYTHON)
 
 def random_color():
@@ -42,14 +43,18 @@ def predict(imgpath):
 parser = argparse.ArgumentParser()
 parser.add_argument('--img', help='path to image')
 parser.add_argument('--stepsize', type=int, default=512, help='sliding window stepsize, to be set to image input size')
-parser.add_argument("--model-dir",help="model directory")
+parser.add_argument('--cropsize', type=int, default=512, help='sliding window cropsize, to be set to image input size')
+parser.add_argument("--model_dir",help="model directory")
 parser.add_argument("--nclasses", type=int, default=3, help="number of classes")
+parser.add_argument("--store_crops_and_masks",action='store_true')
+parser.add_argument("--dir_sv",type=str,default='./')
+parser.add_argument("--detect_threshold",type=int,default=0)
 args = parser.parse_args()
 
 # creating ML service
 model_repo = args.model_dir
 model = {'repository':model_repo}
-parameters_input = {'connector':'image','width':args.stepsize,'height':args.stepsize, 'scale': 0.0039}
+parameters_input = {'connector':'image','width':args.cropsize,'height':args.cropsize, 'scale': 0.0039}
 parameters_mllib = {'nclasses':args.nclasses,'segmentation':True,'gpu':True,'gpuid':0}
 parameters_output = {}
 try:
@@ -59,7 +64,6 @@ except: # most likely the service already exists
     pass
 
 img = cv2.imread(args.img)
-print('image shape=',img.shape)
 
 # visual output
 label_colours = []
@@ -85,7 +89,7 @@ for (x, y, window) in sliding_window(img, stepSize=args.stepsize, windowSize=(ar
         window = windowfull
     
     # - get the local image window
-    windowpath = '/tmp/seg/img'+str(i)+'.jpg' 
+    windowpath = os.path.join(args.dir_sv,'tmp/seg/img'+str(i)+'.jpg')
     cv2.imwrite(windowpath, window)
 
     # - process with DD
@@ -96,10 +100,11 @@ for (x, y, window) in sliding_window(img, stepSize=args.stepsize, windowSize=(ar
     r = pixels.copy()
     g = pixels.copy()
     b = pixels.copy()
-    for l in range(0,args.nclasses):
-        r[pixels==l] = label_colours[l,0]
-        g[pixels==l] = label_colours[l,1]
-        b[pixels==l] = label_colours[l,2]
+    if not args.store_crops_and_masks:
+        for l in range(0,args.nclasses):
+            r[pixels==l] = label_colours[l,0]
+            g[pixels==l] = label_colours[l,1]
+            b[pixels==l] = label_colours[l,2]
     r = np.reshape(r,(imgsize['height'],imgsize['width']))
     g = np.reshape(g,(imgsize['height'],imgsize['width']))
     b = np.reshape(b,(imgsize['height'],imgsize['width']))
@@ -108,12 +113,26 @@ for (x, y, window) in sliding_window(img, stepSize=args.stepsize, windowSize=(ar
     rgb[:,:,1] = g
     rgb[:,:,2] = b
 
-    # - combine the output maps
     if resized:
         rgb = rgb[0:windowtmp.shape[0],0:windowtmp.shape[1]]
+
+    if args.store_crops_and_masks:
+        img_name=args.img.split('/')[-1].split('.')[0] + '_' + str(i)
+        crop_name=img_name + '_' + str(i)
+        if rgb.sum()>args.detect_threshold: #something is detected
+            cat='detected'
+        else:# there is only background
+            cat='background'
+        path_sv_mask = os.path.join(args.dir_sv,cat,'mask',img_name + '_mask.jpg') #window_path.split('.')[0] + '_mask.jpg' + window_path.split('.')[1]
+        path_sv_img = os.path.join(args.dir_sv,cat,'img',img_name + '_img.jpg') #window_path.split('.')[0] + '_mask.jpg' + window_path.split('.')[1]
+        os.path.join(args.dir_sv,cat,img_name) + '_mask.jpg'
+        cv2.imwrite(path_sv_mask, rgb)
+        cv2.imwrite(path_sv_img, windowtmp)
+
+    # - combine the output maps
     segmap[y: y + window.shape[1], x: x + window.shape[0]] = rgb
     
     i += 1
 
 # - save the output map
-cv2.imwrite('outseg.jpg', segmap)
+cv2.imwrite(os.path.join(args.dir_sv,'outseg.jpg'), segmap)
